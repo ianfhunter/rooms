ed563ddf498496a5feef65f82ae8d394a70d99f5
Ian Hunter <ianfhunter@gmail.com>
2025-02-24 13:59:21 +0000

    on loaded

--- assets/js/callouts.js
+++ assets/js/callouts.js
@@ -1,18 +1,20 @@
 // add icon and label to parent
 
-const tldrContent = '<i class="fa fa-bolt"></i>monster:'
-const infoContent = '<i class="fa fa-info-circle"></i>item:'
-
-const targets = document.querySelectorAll('.monster, .item')
-
-targets.forEach(parent => {
-  const child = document.createElement('span')
-  child.classList.add('icon')
-  if (parent.classList.contains('monster')) {
-    child.innerHTML = tldrContent
-    parent.prepend(child)
-  } else if (parent.classList.contains('item')) {
-    child.innerHTML = infoContent
-    parent.prepend(child)
-  }
-})
+document.addEventListener('DOMContentLoaded', () => {
+    const tldrContent = '<i class="fa fa-bolt"></i>monster:'
+    const infoContent = '<i class="fa fa-info-circle"></i>item:'
+  
+    const targets = document.querySelectorAll('.monster, .item')
+  
+    targets.forEach(parent => {
+      const child = document.createElement('span')
+      child.classList.add('icon')
+      if (parent.classList.contains('monster')) {
+        child.innerHTML = tldrContent
+        parent.prepend(child)
+      } else if (parent.classList.contains('item')) {
+        child.innerHTML = infoContent
+        parent.prepend(child)
+      }
+    })
+  })
