4f37e21f9249596c203819c523c40aef287c93d2
Ian Hunter <ianfhunter@gmail.com>
2025-02-24 13:55:19 +0000

    simplify

--- assets/js/callouts.js
+++ assets/js/callouts.js
@@ -1,17 +1,17 @@
 // add icon and label to parent
 
-const tldrContent = '<i class="fa fa-bolt"></i>tldr:'
-const infoContent = '<i class="fa fa-info-circle"></i>info:'
+const tldrContent = '<i class="fa fa-bolt"></i>monster:'
+const infoContent = '<i class="fa fa-info-circle"></i>item:'
 
-const targets = document.querySelectorAll('.tldr, .info')
+const targets = document.querySelectorAll('.monster, .item')
 
 targets.forEach(parent => {
   const child = document.createElement('span')
   child.classList.add('icon')
-  if (parent.classList.contains('tldr')) {
+  if (parent.classList.contains('monster')) {
     child.innerHTML = tldrContent
     parent.prepend(child)
-  } else if (parent.classList.contains('info')) {
+  } else if (parent.classList.contains('item')) {
     child.innerHTML = infoContent
     parent.prepend(child)
   }
